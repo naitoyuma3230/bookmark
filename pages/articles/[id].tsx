1749e4875d6c53cd7d1416512b63f18e0de45f50
内藤祐馬 <naitoyuma@outlook.jp>
2023-02-16 13:25:23 +0900

    index->article

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -1,5 +1,29 @@
+import { GetServerSideProps } from 'next'
+import { getSession } from 'next-auth/react'
+import prisma from '../../lib/prisma'
+
 const Article = () => {
-  return <h1>article</h1>
+  return <div>article</div>
 }
 
 export default Article
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+  res,
+}) => {
+  const session = await getSession({ req })
+  if (!session) {
+    res.statusCode = 401
+    return { props: { article: null } }
+  }
+  const article = await prisma.article.findUnique({
+    where: {
+      id: Number(params?.id),
+    },
+  })
+  return {
+    props: { article },
+  }
+}
