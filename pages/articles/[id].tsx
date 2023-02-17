0129d811313c33b1b9522241a42a61197d917922
内藤祐馬 <naitoyuma@outlook.jp>
2023-02-17 16:43:30 +0900

    add, remove bookmark

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -1,12 +1,30 @@
 import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/react'
 import prisma from '../../lib/prisma'
-import { articles } from '../../data/articles'
 import { ArticleProps } from '../../types/Article'
+import { User } from '../../types/User'
+import Router from 'next/router'
 
 type Props = {
   article: ArticleProps
   isBookmarked: boolean
+}
+
+async function addBookmark(id: number): Promise<void> {
+  await fetch(process.env.NEXT_PUBLIC_VERCEL_URL + `/api/bookmark/add/${id}`, {
+    method: 'PUT',
+  })
+  Router.push(`/articles/${id}`)
+}
+
+async function removeBookmark(id: number): Promise<void> {
+  await fetch(
+    process.env.NEXT_PUBLIC_VERCEL_URL + `/api/bookmark/remove/${id}`,
+    {
+      method: 'PUT',
+    }
+  )
+  Router.push(`/articles/${id}`)
 }
 
 const Article = (props: Props) => {
@@ -20,9 +38,11 @@ const Article = (props: Props) => {
             {props.article.content}
           </p>
           {props.isBookmarked ? (
+            // ブックマークされている場合には、ブックマークを削除するボタンを設置します
             <button
               type='button'
               className='mt-5 inline-flex items-center rounded-lg bg-red-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800'
+              onClick={() => removeBookmark(props.article.id)}
             >
               Remove Bookmark
               <span className='ml-2 inline-flex h-4 w-4 items-center justify-center rounded-full bg-red-200 text-xs font-semibold text-red-800'>
@@ -30,9 +50,11 @@ const Article = (props: Props) => {
               </span>
             </button>
           ) : (
+            // ブックマークされていない場合には、ブックマークするボタンを設置します
             <button
               type='button'
               className='mt-5 inline-flex items-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+              onClick={() => addBookmark(props.article.id)}
             >
               Bookmark this article
               <span className='ml-2 inline-flex h-4 w-4 items-center justify-center rounded-full bg-blue-200 text-xs font-semibold text-blue-800'>
@@ -45,33 +67,33 @@ const Article = (props: Props) => {
     </div>
   )
 }
+
 export default Article
 
-// 記事一覧はGetStaticPropsでビルド時に対し
-// getServerSideProps: リクエスト毎にデータを取得するメソッド
 export const getServerSideProps: GetServerSideProps = async ({
   req,
   params,
   res,
 }) => {
-  // session(cookie)からreqパラムの値を取得
   const session = await getSession({ req })
   if (!session) {
-    res.statusCode = 401
+    res.statusCode = 403
     return { props: { article: null } }
   }
-  // reqにパラムが含まれていれば、articleを取得
-  const article = await prisma.article.findUnique({
-    // ルート上のparams(id)と一致するものをUnique検索
+  const data = await prisma.article.findUnique({
     where: {
       id: Number(params?.id),
     },
-    // 更に一致した記事に紐づくuserを取得(defaultで各テーブルfalse)
     include: {
       users: true,
     },
   })
+
+  const article = JSON.parse(JSON.stringify(data))
+  const isBookmarked = article.users.some(
+    (user: User) => user.email === session.user?.email
+  )
   return {
-    props: { article },
+    props: { article, isBookmarked },
   }
 }
