4554e149ffb2e5739c7792781c156d512f1d0564
内藤祐馬 <naitoyuma@outlook.jp>
2023-02-20 16:36:09 +0900

    create Mypage

--- pages/api/bookmark/add/[id].ts
+++ pages/api/bookmark/add/[id].ts
@@ -2,13 +2,13 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../../lib/prisma'
 import { getSession } from 'next-auth/react'
 
-// リクエストとレスポンスの型を指定しています
+// res, reqを引数にもつ関数を定義する
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-  // ここで、req のオブジェクトから認証情報を取得しています
+  // 認証情報の取得
   const session = await getSession({ req })
 
-  // session オブジェクトに email が存在しているかを判定しています
-  // Prisma によるスキーマの作成を行なった際に、users テーブルの email カラムは必須にしたので、ログインしていれば email にはメールアドレスが格納されているはずです
+  // session オブジェクトに email が存在しているかを判定
+  // users テーブルの email カラムは必須
   if (session?.user?.email) {
     const result = await prisma.article.update({
       where: {
